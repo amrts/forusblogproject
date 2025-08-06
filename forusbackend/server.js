6dc054c15750d356958bff787cb6d46c0ff14013
Amritash Mishra <amritashm59@gmail.com>
2025-08-06 09:51:43 +0530

    cleaned up

--- forusbackend/server.js
+++ forusbackend/server.js
@@ -3,28 +3,28 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 
-// Load environment variables
+
 dotenv.config();
 
-// Initialize express app
+
 const app = express();
 
-// Middleware
-app.use(cors());                // Allow frontend to access backend
-app.use(express.json());        // Parse JSON from request body
 
-// Import routes
+app.use(cors());               
+app.use(express.json());       
+
+
 const userRoutes = require('./routes/userRoutes');
 const postRoutes = require('./routes/postRoutes');
 
-// Use routes
+
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 
-// Connect to MongoDB
+
 const connectDB = require('./config/db');
 connectDB();
 
-// Start server
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
